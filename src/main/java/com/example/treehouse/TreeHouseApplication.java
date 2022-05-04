e63ae8b8f3d51c9df3e04cea77390d21937ee969
decagon <godsonjonathan39@gmail.com>
2022-05-04 23:54:07 +0100

    Project Set Up

---
+++ src/main/java/com/example/treehouse/TreeHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.treehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeHouseApplication.class, args);
+    }
+
+}
